77613321ef817fe2e8946e984f51a6dcbd638b3c
juumilani <settieyeah@gmail.com>
2017-05-16 11:47:56 -0300

    o berro que eu dei
    DEU CERTO TD ESSAS MERDA AE PORRA CARALHO VIADO

--- Code/fila.h
+++ Code/fila.h
@@ -1,152 +1,196 @@
 #ifndef FilaClass
 #define Filaclass
-#include <iostream>
-using namespace std;
 
-class Fila {
-	public:	
-		Fila(int);
-		~Fila();
-		bool Insere(char);
-		char Retira(bool&);
-		bool Vazia();
-		bool Cheia();
-		bool Igual(Fila&, Fila&, int);
-		int Contador(Fila&);
-		
-	private:
-		// do, do#, re, re#, mi, fa, fa#, sol, sol#, la, la#, si [1 a 13] //não usaremos 0 senão a lógica não funciona
-		char *array;
-		int frente;   //frente == primeiro elemento
-		int tras;	  //tras   == ultimo elemento
-		int tam;
-		
-		
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+bool deuCerto = true;
+
+struct node{
+
+    char info;
+
+    struct node *next;
+
 };
 
-Fila::Fila(int _tam){
-	int i;
-	tam = _tam;
-	array = new char[tam];
-	frente = 0;
-	tras = tam;
-	
-	for (i = 0; i< tam, i++){
-		array[i] = "";
-	}
-}
+class Fila{
+
+    private:
+
+        node *tras;
+
+        node *frente;
+
+    public:
+
+        Fila();
+
+        void Insere(char);
+
+        char Retira();
+        
+        bool Vazia();
+        
+        bool Compara(Fila&, Fila&);
+        
+        int Contador();
+        
+        
+        
 
 
-Fila::~Fila(){
-	delete [] array;
-}
+};
 
-bool Fila::Insere(char nota){
-	if (!Fila::Cheia()) {
-		array[tras] = nota;
-		tras = (tras + 1)% tam;
-		return true;
-	}
-	else
-		return false;
-}
+Fila::Fila(){
 
-char Fila::Retira(bool &deuCerto){
-	char aux = "";
-	if (!Fila::Vazia()){
-		aux = array[frente];
-		frente = (frente + 1)% tam;
-		return aux;
-		deuCerto = true;
-	}
-	else
-		deuCerto = false;
+    tras = NULL;
+    frente = NULL;
+
 }
 
 bool Fila::Vazia(){
-	if (frente == tras)
-		return true;
+	if (tras == NULL && frente == NULL)
+		return 1;
 	else
-		return false;
+		return 0;
 }
 
-bool Fila::Cheia(){
-	if ((tras + 1)% tam == frente)
-		return true;
-	else
-		return false;
+void Fila::Insere(char nota){
+
+    node *temp = new node;
+    
+    if (Fila::Vazia() == 1){
+    	temp->info = nota;
+    	frente = temp;
+    	tras = temp;
+    	
+    	
+	} 
+    
+    if (Fila::Vazia() != 1){
+	
+    	temp->info = nota;
+    	temp->next = frente;
+    	tras->next = temp;
+    	tras = temp;
+	} 
+
 }
 
+char Fila::Retira(){
 
-bool Fila::Igual(Fila &f1, Fila &f2, int _tam){
-	bool equalForNow;
-	char deuCerto1, deuCerto2, aux;
-	Fila fAux1(50);
-	Fila fAux2(50);
-	
-	
-	equalForNow = true;
-	
-	while ((f1.Vazia() == false) && (f2.Vazia() == false)) {
-		deuCerto1 = f1.Retira();
-		deuCerto2 = f2.Retira();
-		if (deuCerto1 != "")
-			fAux1.Insere(deuCerto1);
-		if (deuCerto2 != "")
-			fAux2.Insere(deuCerto2);
-		//isso aqui checou se o número de elementos das filas era igual
-		
-		if ((deuCerto1 != "") && (deuCerto2 == "")){
-			equalForNow = false;
+    node *temp = new node;
+    char x;
+    
+    if (Fila::Vazia() != 1){
+    	if (tras == frente){
+    		x = tras->info;
+    		frente = NULL;
+    		tras = NULL;
+    		
+    		return x;
+    		
+		} else if (frente->next = tras){
+			x = frente->info;
+			temp = frente;
+			frente = tras;
+			delete temp;
+			
+			return x;
+		} else {
+			temp = tras;
+    	x = tras->info;
+    	tras->next = frente->next;
+    	frente->next = frente;
+    	
+    	return x;
+    	
+    	delete temp;
 		}
-		if ((deuCerto1 == "") && (deuCerto2 != "")){
-			equalForNow = false;
-		}
-		if ((deuCerto1 != "") && (deuCerto2 != "")){
-			if (deuCerto1 != deuCerto2){
-				equalForNow = false;
-			}
-		}
-		
-	} //checagem feita
+	} else
+		deuCerto = false;
+
+}
+
+bool Fila::Compara(Fila &f1, Fila &f2){
 	
-	//voltando os elementos para suas filas
-	while (fAux1.Vazia() != false){
+	node *temp1 = new node;
+	node *temp2 = new node;
+	char aux;
+	int cont1 = 0, cont2 = 0;
+	Fila fAux1;
+	Fila fAux2;
+	bool igual = true;
+	
+	while (f1.Vazia() != 1){
+		aux = f1.Retira();
+		cont1++;
+		fAux1.Insere(aux);
+	}
+	
+	while (f2.Vazia() != 1){
+		aux = f2.Retira();
+		cont2++;
+		fAux2.Insere(aux);
+	}
+	
+	//viu se os tamanhos são iguais
+	
+	while (fAux1.Vazia() != 1){
 		aux = fAux1.Retira();
 		f1.Insere(aux);
 	}
 	
-	while (fAux2.Vazia() != false) {
+	while (fAux2.Vazia() != 1){
 		aux = fAux2.Retira();
 		f2.Insere(aux);
 	}
 	
-	if (equalForNow == true)
-		return true;
-	else
+	
+	if (cont1 == cont2){
+		temp1 = f1.frente;
+		temp2 = f2.frente;
+		do {
+			if (temp2->info == temp1->info){
+				temp1 = temp1->next;
+				temp2 = temp2->next;
+			} else
+				igual = false;
+				
+		} while ((temp1 != f1.tras) && (temp2 != f2.tras));
+	} else
+		igual = false;
+	
+	
+	if (igual == false)
 		return false;
-	
-} 
+	else
+		return true;
+		
+}
 
-// metodo adicionado, pronto para ser usado
 
-int Fila::Contador(Fila &f1){
-	Fila fAux(50);
-	int cont;
-	char x;
+int Fila::Contador(){
 	
-	while (f1.Vazia() != 1){
-		x = f1.Retira();
+	Fila fAux;
+	int cont = 0;
+	char aux;
+	
+	while (this->Vazia() != 1){
+		aux = this->Retira();
 		cont++;
-		fAux.Insere(x);
+		fAux.Insere(aux);
 	}
 	
 	while (fAux.Vazia() != 1){
-		x = fAux.Retira();
-		f1.Insere(x);
+		aux = fAux.Retira();
+		this->Insere(aux);
 	}
 	
 	return cont;
 }
+
+
 
 #endif
